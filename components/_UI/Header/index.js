3b86cb9613d5050960fe208e1fcbbdceef0ec8a0
Jardel Gonçalves Ferreira <jardelgoncalves1996@gmail.com>
2020-04-17 00:04:32 -0300

    feat: add header component and style :sparkles:

---
+++ components/_UI/Header/index.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './style.css';
+import logo from '../../../public/static/images/fashionista_logo.png';
+
+const Header = ({ onSearch, onShoppingCart, productInCart }) => {
+  return (
+    <header className="header">
+      <div className="logo">
+        <img src={logo} alt="Logo" />
+      </div>
+      <ul className="header__menu">
+        <li>
+          <button type="button" onClick={onSearch}>
+            <i className="fa fa-search" />
+          </button>
+        </li>
+        <li>
+          <button type="button" onClick={onShoppingCart}>
+            <i className="fa fa-shopping-bag" />
+            {Boolean(productInCart) && (
+              <div className="badges">{productInCart}</div>
+            )}
+          </button>
+        </li>
+      </ul>
+    </header>
+  );
+};
+
+Header.propTypes = {
+  onSearch: PropTypes.func,
+  onShoppingCart: PropTypes.func,
+  productInCart: PropTypes.number,
+};
+
+Header.defaultProps = {
+  onSearch: () => null,
+  onShoppingCart: () => null,
+  productInCart: 0,
+};
+
+export default Header;
