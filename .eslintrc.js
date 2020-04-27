f5268401de3ca18c2a3906e66400cfdeed9a8b81
Jardel Gonçalves Ferreira <jardelgoncalves1996@gmail.com>
2020-04-26 23:25:57 -0300

    refactor: disabled click-events-have-key :recycle:

--- .eslintrc.js
+++ .eslintrc.js
@@ -35,6 +35,8 @@ module.exports = {
     'react/state-in-constructor': 'off',
     'react/jsx-props-no-spreading': 'off',
     'camelcase': 'off',
+    'jsx-a11y/no-static-element-interactions': 'off',
+    'jsx-a11y/click-events-have-key-events': 'off',
     'jsx-a11y/anchor-is-valid': 'off', // Link component of Next.js
   },
   overrides: [
