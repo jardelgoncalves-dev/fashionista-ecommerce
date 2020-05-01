883754cf623c1828de4c4c42f61b090effb2b924
Jardel Gonçalves Ferreira <jardelgoncalves1996@gmail.com>
2020-04-30 22:52:39 -0300

    feat: add support svg images :sparkles:

---
+++ next.config.js
@@ -0,0 +1,17 @@
+const withImages = require('next-images');
+
+module.exports = withImages({
+  exportPathMap() {
+    return {
+      '/': { page: '/' },
+    };
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
+});
