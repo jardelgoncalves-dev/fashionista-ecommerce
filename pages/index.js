e51556f79259e1a7db9aba5f0281731b75826219
Jardel Gonçalves Ferreira <jardelgoncalves1996@gmail.com>
2020-04-30 23:43:38 -0300

    refactor: add home page with mock

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { Products } from '../components/_UI';
 
-import { Head } from '../components/Page';
+const data = [
+  {
+    name: 'Blusa Red Stripes',
+    actual_price: 'R$ 149,90',
+    image:
+      'https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcQ4HZsF8oNnV0KcrmM6Fv7oJ3usRcFxPMCrkqr-fkKLW09rPS0&usqp=CAY',
+    discount_percentage: '20%',
+    path: '/',
+  },
+];
 
-const pages = () => (
-  <>
-    <Head />
-    <h1>Hello</h1>
-  </>
-);
+const Home = () => <Products data={data} />;
 
-export default pages;
+export default Home;
