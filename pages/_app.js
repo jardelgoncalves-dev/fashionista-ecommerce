4733b1a757fe544deb37799c2df07c623c1c52e3
Jardel Gonçalves Ferreira <jardelgoncalves1996@gmail.com>
2020-04-30 23:43:17 -0300

    refactor: add common pages component :recycle:

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,21 @@
 import App from 'next/app';
 import React from 'react';
 
+import { Head, Layout } from '../components/Page';
+import { Header } from '../components/_UI';
+
 export default class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
 
-    return <Component {...pageProps} />;
+    return (
+      <>
+        <Head />
+        <Header />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </>
+    );
   }
 }
