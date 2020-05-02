332380cab8d70713ba90b548d6238586b3de0ec5
Jardel Gonçalves Ferreira <jardelgoncalves1996@gmail.com>
2020-05-02 15:51:54 -0300

    refactor: redux support :recycle:

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,37 @@
 import App from 'next/app';
 import React from 'react';
+import { Provider } from 'react-redux';
+import withRedux from 'next-redux-wrapper';
 
 import { Head, Layout } from '../components/Page';
 import { Header } from '../components/_UI';
 
-export default class MyApp extends App {
+import store from '../store';
+
+class MyApp extends App {
+  static async getInitialProps({ Component, ctx }) {
+    const pageProps = Component.getInitialProps
+      ? await Component.getInitialProps(ctx)
+      : {};
+
+    return { pageProps };
+  }
+
   render() {
     const { Component, pageProps } = this.props;
 
     return (
-      <>
+      <Provider store={store}>
         <Head />
         <Header />
         <Layout>
           <Component {...pageProps} />
         </Layout>
-      </>
+      </Provider>
     );
   }
 }
+
+const makeStore = () => store;
+
+export default withRedux(makeStore)(MyApp);
