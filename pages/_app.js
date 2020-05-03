fd8a363354dfc2de8af8691d9c8a0ba3c688cfde
Jardel Gonçalves Ferreira <jardelgoncalves1996@gmail.com>
2020-05-03 01:59:52 -0300

    feat: add drawer :sparkles:

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,19 @@
 import App from 'next/app';
 import React from 'react';
-import { Provider } from 'react-redux';
+import { Provider, connect } from 'react-redux';
 import withRedux from 'next-redux-wrapper';
 
 import { Head, Layout } from '../components/Page';
-import { Header } from '../components/_UI';
+import { Header, Drawer, Bag } from '../components/_UI';
 
 import store from '../store';
 
 class MyApp extends App {
+  state = {
+    modalVisible: false,
+    typeModal: '',
+  };
+
   static async getInitialProps({ Component, ctx }) {
     const pageProps = Component.getInitialProps
       ? await Component.getInitialProps(ctx)
@@ -17,16 +22,53 @@ class MyApp extends App {
     return { pageProps };
   }
 
+  onModalShoppingCart = () => {
+    this.setState({
+      modalVisible: true,
+      typeModal: 'cart',
+    });
+  };
+
+  onModalSearch = () => {
+    this.setState({
+      modalVisible: true,
+      typeModal: 'search',
+    });
+  };
+
+  onModalClose = () => {
+    this.setState({
+      modalVisible: false,
+      typeModal: '',
+    });
+  };
+
   render() {
-    const { Component, pageProps } = this.props;
+    const { Component, pageProps, cart } = this.props;
+
+    const cartSize = cart.reduce((acc, p) => p.amount + acc, 0);
 
     return (
       <Provider store={store}>
         <Head />
-        <Header />
+        <Header
+          onShoppingCart={this.onModalShoppingCart}
+          onSearch={this.onModalSearch}
+        />
         <Layout>
           <Component {...pageProps} />
         </Layout>
+        <Drawer
+          visible={this.state.modalVisible}
+          title={
+            this.state.typeModal === 'cart'
+              ? `Sacola (${cartSize})`
+              : 'Buscar produto'
+          }
+          onClose={this.onModalClose}
+        >
+          <Bag isSearch={this.state.typeModal === 'search'} />
+        </Drawer>
       </Provider>
     );
   }
@@ -34,4 +76,9 @@ class MyApp extends App {
 
 const makeStore = () => store;
 
-export default withRedux(makeStore)(MyApp);
+export default withRedux(makeStore)(
+  connect((state) => ({
+    cart: state.cart,
+    products: state.product,
+  }))(MyApp)
+);
