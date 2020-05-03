90773faa60c4538d4440063fd53bce029e38a086
Jardel Gonçalves Ferreira <jardelgoncalves1996@gmail.com>
2020-05-03 01:46:54 -0300

    refactor: disabling no-param-reassign :recycle:

--- .eslintrc.js
+++ .eslintrc.js
@@ -38,6 +38,7 @@ module.exports = {
     'jsx-a11y/no-static-element-interactions': 'off',
     'jsx-a11y/click-events-have-key-events': 'off',
     'jsx-a11y/anchor-is-valid': 'off', // Link component of Next.js
+    'no-param-reassign': 'off',
   },
   overrides: [
     {
