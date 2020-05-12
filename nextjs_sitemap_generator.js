b1c925c035e063fc0235cac464c179720df645dc
Jardel Gonçalves Ferreira <jardelgoncalves1996@gmail.com>
2020-05-12 01:04:59 -0300

    refactor: move sitemap for public :recycle:

--- nextjs_sitemap_generator.js
+++ nextjs_sitemap_generator.js
@@ -3,5 +3,5 @@ const sitemap = require('nextjs-sitemap-generator');
 sitemap({
   baseUrl: 'https://www.jardelgoncalves-fashionista.netlify.app',
   pagesDirectory: `${__dirname}/pages`,
-  targetDirectory: `${__dirname}/public/static/`,
+  targetDirectory: `${__dirname}/public/`,
 });
